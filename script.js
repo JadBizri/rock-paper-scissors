b4f11e32d3ff4c79fd067d933750e0b2c52a5dcb
Jad Bizri <91910175+JadBizri@users.noreply.github.com>
2023-07-06 21:16:52 -0400

    Add game() function that start a game.
    The game has 5 rounds and displays the winner by the end of it.
    Tested thoroughly using console by different outputs.
    Console sends Error when prompt is cancelled by user, although the
    promt is only temporary for now. Main game is fully functional.

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function getComputerChoice(){
 
 /*
     playRound() Function that starts a round of the game and returns the winner
-    @Return String To let user know if they won or not
+    @Return String To let user know if they won the round or not
 */
 function playRound(playerSelection, computerSelection){
     //toUpperCase() to correctly compare both strings
@@ -24,7 +24,7 @@ function playRound(playerSelection, computerSelection){
     
     //if both have the same weapon, then tie
     if(player === computer){
-        return "It's a Tie!";
+        return "\tIt's a Tie!";
     }
     //if user has rock...
     else if(player === 'ROCK'){
@@ -58,8 +58,52 @@ function playRound(playerSelection, computerSelection){
         return "Error: Player did not choose weapon correctly."
 }
 
-const player = 'paer';
-const computer = getComputerChoice();
-console.log("Player: " + player);
-console.log("Computer: " + computer);
-console.log(playRound(player, computer));
+/*
+    game() Function that starts a new game and returns the winner of the entire game
+    @Return String To let user know if they won the game or not
+*/
+function game(){
+    let playerPoints = 0; //to hold player points
+    let computerPoints = 0; //to hold computer points
+
+    //loop 5 times for 5 rounds per game
+    for(let i = 0; i < 5; i++){
+        let player = prompt("Rock, Paper, or Scissors?"); //get user input
+        let computer = getComputerChoice(); //get computer input
+
+        console.log("Round " + (i + 1)); //print round number
+        let s = playRound(player, computer); //start round
+        console.log(s); //display winner of round
+        //if player wins, then add point
+        if(s.includes('Win'))
+            playerPoints++;
+        //if computer wins, then add point
+        else if(s.includes('Lose'))
+            computerPoints++;
+        //if tie, add point to both
+        else if(s.includes('Tie')){
+            computerPoints++;
+            playerPoints++;
+        }
+        //if error, then error
+        else
+            return;
+        console.log("Player " + playerPoints + " - " + computerPoints + " Computer");
+    }
+    //end loop
+
+    //if player points is greater than computer points, then player wins
+    if(playerPoints > computerPoints){
+        return "You are the Winner!!! :D";
+    }
+    //if computer points is greater than player points, then computer wins
+    else if(playerPoints < computerPoints){
+        return "Better Luck Next Time! :(";
+    }
+    //else, it is a tie (should not be possible)
+    else
+        return "It's a tie..."
+}
+
+//start game
+console.log(game());
